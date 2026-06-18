3c824e8638e3f3f897e06990b4e7eb8050fd0511
oplustil-prog <oplustil@kaiserservis.cz>
2026-06-18 11:59:40 +0200

    Add Supabase production config

---
+++ supabase-config.js
@@ -0,0 +1,9 @@
+window.kaiserSupabaseDefaults = {
+  url: "https://fhjcadhvfutipwbvdzik.supabase.co",
+  anonKey: "sb_publishable_r_lxnCz4v0T2jikoxJigmw_A1U0F5of",
+  table: "kaiser_app_state",
+  rowId: "production",
+  bucket: "kaiser-documents",
+  autoLoad: true,
+  autoSync: false
+};
