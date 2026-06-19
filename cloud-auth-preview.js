bc45c696b7f9bb7a9cbbc75467dd055eee1bcd6c
oplustil-prog <oplustil@kaiserservis.cz>
2026-06-19 10:15:35 +0200

    Load invite script hotfix version

--- cloud-auth-preview.js
+++ cloud-auth-preview.js
@@ -335,7 +335,7 @@
     if (window.kaiserUserInvitesRequested || document.querySelector('script[src*="user-invites"]')) return;
     window.kaiserUserInvitesRequested = true;
     const script = document.createElement("script");
-    script.src = "./user-invites.js?v=20260619-50";
+    script.src = "./user-invites.js?v=20260619-51";
     script.defer = true;
     script.onerror = () => {
       showNotice({
