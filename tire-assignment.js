ae7391b064a09335d7995d426b64f29b7b38efd8
oplustil-prog <oplustil@kaiserservis.cz>
2026-06-20 07:12:40 +0200

    Clarify empty and problem tire positions

--- tire-assignment.js
+++ tire-assignment.js
@@ -1,5 +1,5 @@
 (function () {
-  const BUILD = "20260620-30";
+  const BUILD = "20260620-31";
   const NOTE = "Jednoduche prirazovani pneu primo z mapy osazeni vozidla.";
   const VERSION = "v0.9.12";
 
